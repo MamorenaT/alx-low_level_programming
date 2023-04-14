6736ba90cfaa0ab82617571299b2f0cdcc14c7f6
MamorenaT <mamorenakelebogilemolotsi3@gmail.com>
2023-04-14 15:44:10 -0700

    100-elf_header.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+* create_file - Creates a file.
+* @filename: name of file
+* @text_content: contents to be written to the file.
+* Return: 1 if the file is present, -1 if it fails to return file.
+*  */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, wr, len = 0;
+
+	if (filename == NULL)
+	return (-1);
+
+	if (text_content != NULL)
+	{
+	for (len = 0; text_content[len];)
+	len++;
+	}
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	wr = write(fd, text_content, len);
+
+	if (fd == -1 || wr == -1)
+	return (-1);
+
+	close(fd);
+
+	return (1);
+}
